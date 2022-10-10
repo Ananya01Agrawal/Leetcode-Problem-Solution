ed7ea4a216cabf136bb65490991ff8584a712d09
Ananya Agrawal <99130567+Ananya01Agrawal@users.noreply.github.com>
2022-10-10 22:56:21 +0530

    Create 2206. Divide Array Into Equal Pairs.java

---
+++ 2206. Divide Array Into Equal Pairs.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean divideArray(int[] nums) {
+        Arrays.sort(nums); //sorting the array
+        for(int i=0;i<nums.length;i+=2){
+            if(nums[i]!=nums[i+1])
+                return false;
+        }
+        return true;
+    }
+}
