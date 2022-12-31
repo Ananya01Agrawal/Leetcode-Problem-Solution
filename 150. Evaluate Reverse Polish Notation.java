405786d0c3e045c7f4c413b206b10a83b54fdd9d
Ananya Agrawal <99130567+Ananya01Agrawal@users.noreply.github.com>
2023-01-01 02:18:18 +0530

    Update 150. Evaluate Reverse Polish Notation.java

--- 150. Evaluate Reverse Polish Notation.java
+++ 150. Evaluate Reverse Polish Notation.java
@@ -1,14 +1,34 @@
 class Solution {
     public int evalRPN(String[] tokens) {
-        Stack<Integer> stack = new Stack<>();
-        for (String s : tokens) {
-            if ("+".equals(s)) stack.push(stack.pop() + stack.pop());
-            else if ("-".equals(s)) stack.push(-stack.pop() + stack.pop());
-            else if ("/".equals(s)) stack.push((int)(1D / stack.pop() * stack.pop()));
-            else if ("*".equals(s)) stack.push(stack.pop() * stack.pop());
-            else stack.push(Integer.valueOf(s));
+        Stack<Integer> st = new Stack<>();//creating a stack
+        for(String token:tokens){
+            if("*".equals(token)||"-".equals(token)||"+".equals(token)||"/".equals(token)){
+                int num1=st.pop();
+                int num2=st.pop();
+                st.push(calculate(num1,num2,token));
+            }
+            else{
+                st.push(Integer.parseInt(token));
+            }
         }
+        return st.pop();
+    }
+    private int calculate(int num1,int num2,String token){
+        int ans=0;
+        if("+".equals(token)){
+            ans=num2+num1;
+        }
+        if("*".equals(token)){
+            ans=num2*num1;
+        }
+        if("/".equals(token)){
+            ans=num2/num1;
+        }
+        if("-".equals(token)){
+            ans=num2-num1;
+        }
+        return ans;
 
-        return stack.pop();
     }
 }
+        
