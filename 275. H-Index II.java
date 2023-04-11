757d4ae736a4748a8de34aadec961e1102fcefe5
Ananya Agrawal <99130567+Ananya01Agrawal@users.noreply.github.com>
2023-04-11 09:25:01 +0530

    Create 275. H-Index II.java

---
+++ 275. H-Index II.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        int l=0,r=citations.length-1,h=0;
+        while(l<=r){
+            int mid=(l+r)/2;
+            if(citations.length-mid<=citations[mid]){
+                h=citations.length-mid;
+                r=mid-1;
+            }
+            else{
+                l=mid+1;
+            }
+        }
+        return h;
+    }
+}
+// in this question we are using the binary search
+// time complexity: o(logn)
